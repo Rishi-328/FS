1068b58bfca41f79f0c86cd344f6045f378a56f5
rishika <rishi328g@gmail.com>
2025-03-17 17:41:13 +0530

    day 24

--- Elite/Day24/DefinedEncoding.java
+++ Elite/Day24/DefinedEncoding.java
@@ -40,3 +40,31 @@ import java.util.*;
 //         System.out.println(helper(arr,0));
 //     }
 // }
+import java.util.*;
+public class DefinedEncoding{
+    public static boolean helper(int[] arr){
+        int n = arr.length;
+        int b = 0;
+        int curr = 0;
+        if((arr[0]>>7)==0) b=1;
+        else if(arr[0]>>5 ==6) b=2;
+        else if(arr[0]>>4 == 14) b=3;
+        else if(arr[0]>>3 == 30) b=4;
+        else return false;
+        if(arr.length<b)return false;
+        for(int i=1;i<b;i++){
+            if(arr[i]>>6!=2)return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int len = str.length;
+        int[] arr = new int[len];
+        for(int i=0;i<len;i++){
+            arr[i]=Integer.parseInt(str[i]);
+        }
+        System.out.println(helper(arr));
+    }
+}
